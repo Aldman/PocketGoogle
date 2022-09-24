f1739217add82ec3406b1b85c647472a085ad762
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-09-24 18:17:28 +0300

    Добавьте файлы проекта.

---
+++ Indexer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PocketGoogle
+{
+    public class Indexer : IIndexer
+    {
+        public Dictionary<int, HashSet<string>> Document { get; set; } =
+            new Dictionary<int, HashSet<string>>();
+        private Dictionary<int, string> WholeText { get; set; } =
+            new Dictionary<int, string>();
+
+        public void Add(int id, string documentText)
+        {
+            var splitters = new[] 
+            { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' };
+            var wordsById = documentText.Split(splitters
+                , StringSplitOptions.RemoveEmptyEntries);
+            Document.Add(id, wordsById.ToHashSet());
+            WholeText.Add(id, documentText);
+        }
+
+        // Облегчить
+        public List<int> GetIds(string word)
+        {
+            var result = new List<int>();
+            foreach (var id in Document.Keys)
+            {
+                if (Document[id].Contains(word))
+                    result.Add(id);
+            }
+            return result;
+        }
+
+        // Облегчить
+        public List<int> GetPositions(int id, string word)
+        {
+            var result = new List<int>();
+            var index = 0;
+            while (index >= 0)
+            {
+                index = WholeText[id].IndexOf(word, index);
+                if (index >= 0) result.Add(index);
+            }
+            return result;
+        }
+
+        public void Remove(int id)
+        {
+            Document.Remove(id);
+            WholeText.Remove(id);
+        }
+    }
+}
---
+++ IndexerTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PocketGoogle
+{
+    [TestFixture]
+    internal class IndexerTests
+    {
+        private List<Dictionary<int, string>> examplesForAdding
+        { get; set; } = new List<Dictionary<int, string>>()
+        {
+            new Dictionary<int, string>()
+            {
+                { 1,  "I love you" },
+                { 2, "my sweety kitty" },
+                { 3,  "Anyutya"},
+            },
+            new Dictionary<int, string>()
+            {
+                { 5,  "I am not imagine" },
+                { 3, "my life without you" }
+            },
+            new Dictionary<int, string>()
+            {
+                { 9,  "You are my beautiful girl" }
+            },
+            new Dictionary<int, string>()
+            {
+                { 7,  "swap sweet smeet greet" }
+            }
+        };
+        
+        [Test]
+        public void AddTest()
+        {
+            foreach (var example in examplesForAdding)
+            {
+                var indexer = new Indexer();
+                var expected = FillDictionaryIn(example, indexer);
+                Assert.AreEqual(expected, indexer.Document);
+            }
+        }
+
+        private Dictionary<int, List<string>> FillDictionaryIn(
+            Dictionary<int, string> example, Indexer indexer)
+        {
+            var result = new Dictionary<int, List<string>>();
+            foreach (var id in example.Keys)
+            {
+                indexer.Add(id, example[id]);
+                result.Add(id, example[id].Split(' ').ToList());
+            }
+            return result;
+        }
+
+        [Test]
+        public void GetIdsTest()
+        {
+            var wordParam = "sweety";
+            var expected = new List<int>()
+            {
+                2
+            };
+            var indexer = new Indexer();
+            FillDictionaryIn(examplesForAdding[0], indexer);
+            var actual = indexer.GetIds(wordParam);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetPositionsTest()
+        {
+            var indexer = new Indexer();
+            FillDictionaryIn(examplesForAdding[3], indexer);
+            var expected = new List<int>()
+            {
+                0, 1
+            };
+            var actual = indexer.GetPositions(7, "sw");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
